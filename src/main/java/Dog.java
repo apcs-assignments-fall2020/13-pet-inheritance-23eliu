48c67f0593a522f915a6692f46f6f549b8e9de7a
23eliu <23eliu@athenian.org>
2020-10-30 18:46:26 +0000

    finished dog and dog client

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,9 +1,28 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String a, int b, String c) {
+        super(a, b);
+        this.breed = c;
+    }
+
+    public Dog() {
+        super("Marly", 2);
+        this.breed = "Black Labrador";
+    }
+
+    public Dog(String c) {
+        super();
+        this.breed = c;
+    }
+
+    public String toString() {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+
+        return str;
+    }
 
     // makeNoise() method
 
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,14 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog s1 = new Dog("Mark", 5, "teacup dog");
+        System.out.println(s1);
+
+        Dog s2 = new Dog();
+        System.out.println(s2);
+
+        s1.makeNoise();
+
+        Dog s3 = new Dog("yorkshire terrier");
+        System.out.println(s3);
     }
 }
