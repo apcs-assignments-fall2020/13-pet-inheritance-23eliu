ef01b86925954da9de07697595899a09e01a2ef0
23eliu <23eliu@athenian.org>
2020-11-13 21:50:44 +0000

    added missing methods

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -8,39 +8,30 @@ public class Dog extends Pet {
         this.breed = c;
     }
 
+    // Default Constructor
     public Dog() {
         super("Marly", 2);
         this.breed = "Black Labrador";
     }
 
-    public Dog(String c) {
-        super();
-        this.breed = c;
+    public void makeNoise() {
+        System.out.println("Woof!");
     }
-
+    
     public String toString() {
         String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
-
         return str;
     }
 
-    // makeNoise() method
-
-
-    // toString method
-
-
     // Getter
-    
-    // You were missing the getBreed method, so the tests didn't run
     public String getBreed() {
-        return "missing code";
+        return this.breed;
     }
 
-
     // Setter
-    // You were missing the getBreed method, so the tests didn't run
     public void setBreed(String breed) {
-        String str = "missing code";
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
     }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,14 +1,13 @@
 public class DogClient {
     public static void main(String[] args) {
         Dog s1 = new Dog("Mark", 5, "teacup dog");
-        System.out.println(s1);
 
         Dog s2 = new Dog();
-        System.out.println(s2);
-
+        
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+        System.out.println(s1); 
         s1.makeNoise();
-
-        Dog s3 = new Dog("yorkshire terrier");
-        System.out.println(s3);
+        System.out.println(s2);
     }
 }
